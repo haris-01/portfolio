f5bce124c1b65d5a1e0580acf5f4b9eac9d0eb47
Haris_01 <harissaeed01@outlook.com>
2025-12-23 04:25:05 +0500

    Updates

---
+++ _next/static/BpWfSw76b1QAJppUDSKe8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-32318094bd9f69c5.js"],"/_error":["static/chunks/pages/_error-104932f2b18cf7e8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
