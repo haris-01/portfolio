8df5f5ecb4423397370867f464cc98126006d894
Haris_01 <harissaeed01@outlook.com>
2025-12-23 05:16:23 +0500

    fix deploy,ment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'portfolio';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
