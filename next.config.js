60108438170ce357eff99d5a2699e80cfb3537a8
Haris_01 <harissaeed01@outlook.com>
2025-12-27 01:02:48 +0500

    update next config to remove deployment from local.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'portfolio';
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   trailingSlash: true,
 };
 
