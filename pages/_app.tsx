43e764f9b1382c8321c42d4db637d9be7b5960c9
Haris_01 <harissaeed01@outlook.com>
2025-12-27 01:51:05 +0500

    remove the develop deployment file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,20 +9,6 @@ initializeGSAPPlugins();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {process.env.NODE_ENV === 'development' && (
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            width: '100%',
-            background: 'red',
-            color: 'white',
-            textAlign: 'center',
-          }}
-        >
-          DEV SITE
-        </div>
-      )}
       <MainBackound />
       <Component {...pageProps} />
     </>
